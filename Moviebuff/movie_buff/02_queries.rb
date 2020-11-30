6799e718aefd8c4e731a1b10aab245cc220cd1ab
Yehudah <jros987@gmail.com>
2020-11-30 17:26:45 -0500

    up to 2

--- Moviebuff/movie_buff/02_queries.rb
+++ Moviebuff/movie_buff/02_queries.rb
@@ -2,17 +2,27 @@ def eighties_b_movies
   # List all the movies from 1980-1989 with scores falling between
   # 3 and 5 (inclusive).
   # Show the id, title, year, and score.
-
+Movie 
+  .select(:id, :title, :yr, :score)
+  .where('yr BETWEEN 1980 AND 1989 AND score BETWEEN 3 AND 5')
 end
 
 def bad_years
   # List the years in which a movie with a rating above 8 was not released.
-
+  Movie
+   .select(:yr)
+   .where( 'score < 8 ')
+   .pluck(:yr)
 end
 
 def cast_list(title)
   # List all the actors for a particular movie, given the title.
   # Sort the results by starring order (ord). Show the actor id and name.
+  Actor
+    .select(:name, :id)
+    .joins(:movies)
+    .where('title=?', title)
+    .order('ord ASC')
 
 end
 
@@ -22,6 +32,10 @@ def vanity_projects
   # Show the movie id and title and director's name.
 
   # Note: Directors appear in the 'actors' table.
+ Movie 
+  .select(:id, :title, actors.name)
+  .joins(:director, :actors)
+  .where()
 
 end
 
