3142d1d6e196ce966b49d647753698a3be2cf08a
junghyun2006 <jung.hyun.park516@gmail.com>
2020-11-30 17:44:17 -0500

    Start Vanity Project

--- Moviebuff/movie_buff/02_queries.rb
+++ Moviebuff/movie_buff/02_queries.rb
@@ -10,8 +10,8 @@ end
 def bad_years
   # List the years in which a movie with a rating above 8 was not released.
   Movie
-   .select(:yr)
-   .where( 'score < 8 ')
+   .group(:yr)
+   .having('MAX(score) < 8')
    .pluck(:yr)
 end
 
